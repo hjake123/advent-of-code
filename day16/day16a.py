1b72bf917d85c90fb02e876d207daeb09a26cf38
hjake123 <hjake123@gmail.com>
2023-10-06 14:00:47 -0500

    Day 15 Memoization Part 1 Sample

--- day16/day16a.py
+++ day16/day16a.py
@@ -2,49 +2,57 @@ class ValveNode:
     def __init__(self, label: str, score: int, exits: list):
         self.label = label
         self.children = []
+        self.parent = None
         self.exits = exits
         self.score = score
-    
-    def score_depth_first(self, moves, should_redeem_score = False, parent = None, valves_opened = []) -> int:
+        self.index = 0
+
+    def rdistance(self, other, visited = [], maxdepth = float('inf')):
         '''
-        Returns the best score of any exit from this node
-        Takes into account move limitations.
+        Returns the int distance between this and the other node by scanning our children recursively,
+        or floating point infinity if it can't be found.
         '''
-        base_score = 0
+        if self == other or maxdepth == 0:
+            return 0
+        if get_distance_key(self, other) in distance_cache:
+            return distance_cache[get_distance_key(self, other)]
 
-        if should_redeem_score:
-            moves -= 1
-            valves_opened.append(self)
-            base_score += self.score * moves
-
-        if moves <= 0:
-            return base_score
-        
-        best_score = base_score
-
-        # First, check all children without redeeming the next one's score.
+        dist = float('inf')
         for child in self.children:
-            score = child.score_depth_first(moves-1, parent = self, valves_opened = valves_opened.copy())
-            if score > best_score:
-                best_score = score + base_score
-        
-        # Then, check all children if you do redeeming the next one's score.
-        for child in self.children:
-            if child in valves_opened:
+            if child in visited:
                 continue
-            score = child.score_depth_first(moves-1, should_redeem_score = True, parent = self, valves_opened = valves_opened.copy())
-            if score > best_score:
-                best_score = score + base_score
+            res = child.rdistance(other, visited + [self], min(dist, maxdepth-1)) + 1
+            if res == float('inf'):
+                continue
+            dist = min(res, dist)
+            if dist == 1:
+                return dist
+        return dist
+    
+    def distance(self, other):
+        '''
+        Returns the int distance between this and the other node by scanning our children recursively,
+        or floating point infinity if it can't be found.
+        '''
+        ret = self.rdistance(other)
+        distance_cache.update({get_distance_key(self, other): ret})
+        return ret
 
-        return best_score
+distance_cache = {}
+def get_distance_key(current: ValveNode, other: ValveNode):
+    '''
+    Return the unique state key for this state.
+    '''
+    return current.label + ":" + other.label
 
+valves = []
+nonzero_valves = []
 AA = None
 def read_graph(filename):
     '''
     Read the graph.
     '''
     global AA
-    valves = []
     with open(filename) as file:
         for line in file:
             label = line[6:8]
@@ -57,14 +65,60 @@ def read_graph(filename):
             words = line.split()
             exits = words[9:]
             exits = [exit.strip(',') for exit in exits]
-            valves.append(ValveNode(label, score, exits))
-            if len(valves) == 1:
-                AA = valves[0]
+            valve = ValveNode(label, score, exits)
+            valves.append(valve)
+            if not score == 0:
+                nonzero_valves.append(valve)
+            if AA == None:
+                AA = valve
     
+    i = 0
     for valve in valves:
         for exit in valve.exits:
             child = [v for v in valves if v.label == exit][0]
             valve.children.append(child)
+        valve.index = i
+        i += 1
 
+memos = {}
+def get_state_key(current: ValveNode, past = [], minute = 0):
+    '''
+    Return the unique state key for this state.
+    '''
+    key = current.label + ':'
+    for p in past:
+        key += p.label + ','
+    key += str(minute)
+    return key
+
+def score_state(current: ValveNode, past = [], minute = 0) -> int:
+    '''
+    Choose the next valve to OPEN based on the previous state of affairs. 
+    Uses globals to scan the list of non-zero valves.
+    Uses recursion to determine the best answers, and stores them in the global memos.
+    Returns the best score if you follow this path.
+    '''
+    if minute >= 30:
+        return 0
+
+    if get_state_key(current, past, minute) in memos:
+        return memos[get_state_key(current, past, minute)]
+
+    valves_left = [valve for valve in nonzero_valves if (valve not in past)]
+
+    maximum = 0
+    for next in valves_left:
+        delayed_time = minute + current.distance(next) + 1
+        if delayed_time >= len(max_potentials):
+            continue
+        next_potential = max_potentials[delayed_time][next.index]
+        next_potential += score_state(next, past + [next], delayed_time)
+        if next_potential > maximum:
+            maximum = next_potential
+    memos.update({get_state_key(current, past, minute): maximum})
+    return maximum
+
+# AND NOW, THE SCRIPT ITSELF--!
 read_graph('day16/short.txt')
-print(AA.score_depth_first(30))
+max_potentials = [[max(0, (30-i) * v.score) for v in valves] for i in range(30)]
+print(score_state(AA))
